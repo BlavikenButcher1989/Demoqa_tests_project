cd6ea01fa21e4f8504b601e41fcf9a973057976f
BlavikensButcher1989 <stupid6_stupid@mail.ru>
2025-12-11 12:30:28 +0300

    added base_page.py, conftest.py, requirements.txt

---
+++ pages/base_page.py
@@ -0,0 +1,24 @@
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait as W
+
+
+class BasePage:
+
+    def __init__(self, driver, url):
+        self.driver = driver
+        self.url = url
+
+    def open_page(self):
+        self.driver.get(self.url)
+
+    def element_is_visible(self, locator):
+        return W(self.driver, 10).until(EC.visibility_of_element_located(locator))
+
+    def elements_are_visible(self, locator):
+        return W(self.driver, 10).until(EC.visibility_of_all_elements_located(locator))
+
+    def element_is_clickable(self, locator):
+        return W(self.driver, 10).until(EC.element_to_be_clickable(locator))
+
+    def go_to_element(self, element):
+        self.driver.execute_script("arguments[0].scrollIntoView;", element)
